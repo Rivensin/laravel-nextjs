ce4c15d913af38d49f3a0e7e300225bc37b8245b
riven <rivensinn@gmail.com>
2025-11-14 01:49:03 +0700

    make Product API & Controller

--- api-backend/routes/api.php
+++ api-backend/routes/api.php
@@ -1,20 +1,22 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 
 Route::group([
-    'middleware' => ['auth:sanctum'],
+    'middleware' => ['auth:sanctum']],
     function(){
         Route::get('profile',[AuthController::class,'profile']);
         Route::get('logout',[AuthController::class,'logout']);
-    }
-]);
 
+        Route::apiResource('products', ProductController::class);
+    }
+);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
